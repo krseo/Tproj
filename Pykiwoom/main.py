fcde6256f522ad77cb226a3f8bd3ba4b4c431f92
krseo <krseo@pknu.ac.kr>
2022-08-29 19:34:33 +0900

    아직 알수없음

--- Pykiwoom/main.py
+++ Pykiwoom/main.py
@@ -24,6 +24,30 @@ class MyWindow(QMainWindow, UI_class):
         hts.SetRealReg("1111", "", "20;214;215", "0")
         hts.SetRealReg("1111", "209AK220", "10;20", "1")
 
+        """
+        상장일 = kiwoom.GetMasterListedSTockDate("005690")
+        print(상장일)
+        print(type(상장일))        # datetime.datetime 
+
+
+        전일가 = hts.GetMasterLastPrice("005930")
+        print(int(전일가))
+        print(type(전일가))
+        """
+
+        df = hts.block_request("opt10001",
+                                  종목코드="005930",
+                                  output="주식기본정보",
+                                  next=0)
+        print(df)
+
+        df = hts.block_request("opt10081",
+                                  종목코드="005930",
+                                  기준일자="20200424",
+                                  수정주가구분=1,
+                                  output="주식일봉차트",
+                                  next=0)
+        print(df.head())
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
